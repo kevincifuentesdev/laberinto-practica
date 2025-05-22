1a653d9fe5b75da7140e47ff67cad94ea599509a
kevincifuentesdev <kevins_cifuentes@soy.sena.edu.co>
2025-05-22 18:13:19 -0500

    Fix: queue implementation.

--- persona.py
+++ persona.py
@@ -1,12 +1,13 @@
 from typing import List, Tuple, Optional, Union
 from random import randint
-from collections import deque
+from typing import Any
+from laberinto import Laberinto
 from arbol_general import GeneralTree, Node
 
 MURO = 'X'
 
 class Persona:
-    def __init__(self, posicion_inicial: Tuple[int, int], laberinto) -> None:
+    def __init__(self, posicion_inicial: Tuple[int, int], laberinto: Laberinto) -> None:
         self.laberinto = laberinto
         self.salida = laberinto.salida
         self.posicion_actual: Tuple[int, int] = posicion_inicial
@@ -17,7 +18,6 @@ class Persona:
         self.rutas_tomadas: GeneralTree = self._registrar_movimientos(posicion_inicial)
 
     def _perder_movimiento(self) -> None:
-        # quita un movimiento aleatorio de la lista
         if self.posibles_movimientos:
             idx = randint(0, len(self.posibles_movimientos) - 1)
             self.posibles_movimientos.pop(idx)
@@ -28,34 +28,43 @@ class Persona:
         n = self.laberinto.tamanno
         visited = {start}
         parent: dict = {}
-        queue = deque([start])
+        queue: List[Tuple[int,int]] = [start]
         found = False
+
         while queue:
-            cur = queue.popleft()
+            cur = queue.pop(0)
+
             if cur == end:
                 found = True
                 break
+
             for dx, dy in self.posibles_movimientos:
                 nx, ny = cur[0] + dx, cur[1] + dy
+
                 if 0 <= nx < n and 0 <= ny < n:
                     val = self.laberinto.matriz[ny][nx]
+
                     if val != MURO and (nx, ny) not in visited:
                         visited.add((nx, ny))
                         parent[(nx, ny)] = cur
                         queue.append((nx, ny))
+
         if not found:
             return GeneralTree()
         # reconstruye camino
         path = []
         node = end
+
         while node != start:
             path.append(node)
             node = parent[node]
         path.append(start)
         path.reverse()
+
         # construye árbol lineal de la ruta
         tree = GeneralTree()
         tree.root = Node(start)
+        
         for prev, nxt in zip(path, path[1:]):
             tree.insert(prev, nxt)
         return tree
